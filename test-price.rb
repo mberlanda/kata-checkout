77c1b9516f1ddfdaca5f4fac4b19e930f761c071
Mauro Berlanda <m.berlanda@fit2you.it>
2015-12-04 12:29:25 +0100

    assert A, B and empty items

--- test-price.rb
+++ test-price.rb
@@ -3,20 +3,27 @@ require 'test-unit'
 class CheckOut
 
   def initialize(rules)
-  #
+    @tot = 0
+    @item_hash = {}
   end
 
   def scan(item)
-    @item = item
+    if @item_hash[item]
+      @item_hash[item] += 1
+    else
+      @item_hash[item] = 1
+    end
+
   end
 
   def total
-    case @item
-    when "A"
-      50
-    else
-      0
+    if @item_hash["A"]
+      @tot += 50 * @item_hash["A"]
     end
+    if @item_hash["B"]
+      @tot += 30 * @item_hash["A"]
+    end
+    @tot
 
   end
 
@@ -35,6 +42,8 @@ class TestPrice < Test::Unit::TestCase
   def test_totals
     assert_equal(  0, price(""))
     assert_equal( 50, price("A"))
+    assert_equal( 80, price("AB"))
+
   end
 
 end
