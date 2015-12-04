a1bfc90e7d7ce3a000771d3e8fd982bd944a16a3
Mauro Berlanda <m.berlanda@fit2you.it>
2015-12-04 12:33:21 +0100

    assert ABCD and empty items

--- test-price.rb
+++ test-price.rb
@@ -21,7 +21,13 @@ class CheckOut
       @tot += 50 * @item_hash["A"]
     end
     if @item_hash["B"]
-      @tot += 30 * @item_hash["A"]
+      @tot += 30 * @item_hash["B"]
+    end
+    if @item_hash["C"]
+      @tot += 20 * @item_hash["C"]
+    end
+    if @item_hash["D"]
+      @tot += 15 * @item_hash["D"]
     end
     @tot
 
@@ -43,6 +49,8 @@ class TestPrice < Test::Unit::TestCase
     assert_equal(  0, price(""))
     assert_equal( 50, price("A"))
     assert_equal( 80, price("AB"))
+    assert_equal(115, price("CDBA"))
+
 
   end
 
